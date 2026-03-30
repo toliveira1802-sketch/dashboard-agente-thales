2025f98fef6a0cfc9b488531875d97b13dc0bd03
toliveira1802-sketch <toliveira1802@gmail.com>
2026-03-30 04:10:41 -0300

    feat: dashboard v2.0 - THALES com sidebar, brain animation, chat, se├º├Áes completas

---
+++ do_push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\Users\docto\Downloads\dashboard-agente-thales
+git add -A
+git commit -m "feat: dashboard v2.0 - THALES com sidebar, brain animation, chat, seções completas"
+git push origin main
